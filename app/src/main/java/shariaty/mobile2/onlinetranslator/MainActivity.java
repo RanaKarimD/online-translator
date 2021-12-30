409f733bd05c44dd1e7ef39b8a4c1e4600dcf7c5
Rana <karimirkd99@gmail.com>
2021-12-31 01:09:10 +0330

    fourth commit

--- app/src/main/java/shariaty/mobile2/onlinetranslator/MainActivity.java
+++ app/src/main/java/shariaty/mobile2/onlinetranslator/MainActivity.java
@@ -4,20 +4,51 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlertDialog;
 import android.content.Intent;
+import android.icu.text.Transliterator;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.Spinner;
 
 import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    private     Spinner fromspinner,spinnerto;
+    private TextInputEditText  inputEditText;
+    private Button button;
+    String[] FromLang = {"from","english","persian"};
+    String[] ToLang = {"to","english","persian"};
+    int froml,tol,Cl;
     AlertDialog.Builder alertBuilder;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        spinnerto= findViewById(R.id.spinnerto);
+        fromspinner= findViewById(R.id.fromSpinner);
+        inputEditText= findViewById(R.id.textInput);
+        button= findViewById(R.id.button);
+
+        fromspinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
     }
+
+
 
     public  void onClickinfo(View view){
         alertBuilder = new AlertDialog.Builder(this);
