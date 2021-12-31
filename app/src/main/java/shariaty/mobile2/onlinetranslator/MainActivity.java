987e89a15d0da35a7a377ad86b591a0915e4ae71
Rana <karimirkd99@gmail.com>
2021-12-31 06:11:39 +0330

    apk

--- app/src/main/java/shariaty/mobile2/onlinetranslator/MainActivity.java
+++ app/src/main/java/shariaty/mobile2/onlinetranslator/MainActivity.java
@@ -8,19 +8,21 @@ import android.icu.text.Transliterator;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    private     Spinner fromspinner,spinnerto;
-    private TextInputEditText  inputEditText;
+    private Spinner fromspinner, spinnerto;
+    private TextInputEditText inputEditText;
     private Button button;
-    String[] FromLang = {"from","english","persian"};
-    String[] ToLang = {"to","english","persian"};
-    int froml,tol,Cl;
+    String[] FromLang = {"english", "persian"};
+    String[] ToLang = {"english", "persian"};
+    int froml, tol, Cl;
     AlertDialog.Builder alertBuilder;
 
 
@@ -29,28 +31,40 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        spinnerto= findViewById(R.id.spinnerto);
-        fromspinner= findViewById(R.id.fromSpinner);
-        inputEditText= findViewById(R.id.textInput);
-        button= findViewById(R.id.button);
+        spinnerto = findViewById(R.id.spinnerto);
+        fromspinner = findViewById(R.id.fromSpinner);
+        inputEditText = findViewById(R.id.textInput);
+        button = findViewById(R.id.button);
+
+
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, FromLang);
+        fromspinner.setAdapter(adapter);
+        fromspinner.setAdapter(adapter);
+
+
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, ToLang);
+        spinnerto.setAdapter(adapter2);
+        spinnerto.setAdapter(adapter2);
+
 
         fromspinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long l) {
+               // String item = adapterView.getItemAtPosition(position).toString();
 
             }
 
             @Override
             public void onNothingSelected(AdapterView<?> adapterView) {
 
+                Toast.makeText(adapterView.getContext(), "Select language ", Toast.LENGTH_LONG).show();
             }
         });
     }
 
 
-
-    public  void onClickinfo(View view){
+    public void onClickinfo(View view) {
         alertBuilder = new AlertDialog.Builder(this);
 
         alertBuilder.setTitle("about us");
@@ -62,8 +76,8 @@ public class MainActivity extends AppCompatActivity {
         alert.show();
     }
 
-    public  void onClickGoToWordTranslation (View view){
-        Intent intent=new Intent(this,WordTranslation.class);
+    public void onClickGoToWordTranslation(View view) {
+        Intent intent = new Intent(this, WordTranslation.class);
         startActivity(intent);
     }
 }
